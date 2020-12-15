f0809d7cda99fdabee73bf3766db55ae0cfe955c
Kali <51704336+doyoun0913@users.noreply.github.com>
2020-12-14 23:13:03 -0800

    home.js file

---
+++ home.js
@@ -0,0 +1,6 @@
+function hideAds() {
+  var x = document.getElementById("specialoffers");
+  var y = document.getElementById("hidebutton");
+  x.style.display = "none";
+  y.style.display = "none";
+}
